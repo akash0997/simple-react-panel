20aeb8e4684519f1190204494ffc8acbb387f12b
akash0997 <50735938+akash0997@users.noreply.github.com>
2019-09-27 14:31:33 +0530

    Update module.tsx

--- src/module.tsx
+++ src/module.tsx
@@ -1,10 +1,40 @@
 import React, { PureComponent } from 'react';
-import { PanelProps, PanelPlugin } from '@grafana/ui';
+import { ReactPanelPlugin, PanelOptionsGroup, PanelProps, PanelEditorProps, FormField } from '@grafana/ui';
 
-export class MyPanel extends PureComponent<PanelProps> {
+export class MyPanel extends PureComponent<PanelProps<MyPanelOptions>> {
   render() {
-    return <div>Hello from my panel</div>;
+    const { options } = this.props;
+    return (
+      <div className="center-vh">
+        <h1 style={{ fontSize: '80px' }}>{options.bigText}</h1>
+      </div>
+    );
   }
 }
 
-export const plugin = new PanelPlugin(MyPanel);
+interface MyPanelOptions {
+  bigText: string;
+}
+
+export class MyPanelEditorProps extends PureComponent<PanelEditorProps<MyPanelOptions>> {
+  onBigTextChanged = (evt: { target: { value: string } }) => {
+    this.props.onChange({
+      ...this.props.options,
+      bigText: evt.target.value,
+    });
+  };
+
+  render() {
+    const { bigText } = this.props.options;
+
+    return (
+      <PanelOptionsGroup title="My options">
+        <FormField label="Big text" onChange={this.onBigTextChanged} value={bigText} />
+      </PanelOptionsGroup>
+    );
+  }
+}
+
+export const plugin = new ReactPanelPlugin<MyPanelOptions>(MyPanel);
+plugin.setEditor(MyPanelEditorProps);
+plugin.setDefaults({ bigText: 'Akash' });
